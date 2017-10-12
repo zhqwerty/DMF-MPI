ead9f2bd432279572c4708baed18a1cae5c1aba5
zh <zhanghan213@163.com>
2017-10-12 16:21:04 +0800

    add defines.h

---
+++ defines.h
@@ -0,0 +1,16 @@
+#ifndef _DEFINES_
+#define _DEFINES_
+
+#include <armadillo>
+#include <vector>
+#include <cmath>
+#include <mpi.h>
+#include <iostream>
+
+#include "Example/examples.h"
+#include "Model/model.h"
+#include "Model/model_mf_sig.h"
+#include "Updater/updater.h"
+#include "Train/trainer.h"
+
+#endif
