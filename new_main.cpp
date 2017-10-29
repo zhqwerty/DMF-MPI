4f2d569d7f8de9da8e90894992c0fd54a454d87c
zh <zhanghan213@163.com>
2017-10-29 16:44:42 +0800

    new_main

--- new_main.cpp
+++ new_main.cpp
@@ -1,4 +1,15 @@
-#include "defines.h"
+#include <iostream>
+#include <vector>
+#include <armadillo>
+#include <math.h>
+#include <mpi.h>
+#include "Example/examples.h"
+#include "Updater/updater.h"
+#include "Trainer/trainer.h"
+#include "Trainer/worker_trainer.h"
+#include "Trainer/master_trainer.h"
+#include "Tools/global_macros.h"
+
 using namespace arma;
 
 int main(int argv, char *argc[]){
@@ -7,6 +18,7 @@ int main(int argv, char *argc[]){
     MPI_Init(&argv, &argc);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+ 
     const char* inputTrainFile = "/Users/ZMY/Downloads/Matrix/data/Slashdot/train.txt";
     const char* inputTestFile = "/Users/ZMY/Downloads/Matrix/data/Slashdot/test.txt";
     int nRows, nCols, nExamples;
@@ -17,17 +29,28 @@ int main(int argv, char *argc[]){
     std::cout << "nRows: " << nRows  << " nCols: " << nCols << " nExamples: " << nExamples << std::endl;
     std::cout << "testRows: " << testRows  << " testCols: " << testCols << " testExamples: " << testExamples << std::endl;
 //    for (int i = 0; i < 10; i++) std::cout << trainData[i].row << " " << trainData[i].col << " " << trainData[i].rating << std::endl;
-    
-    Model* model = new Model;
-    Updater* updater = new Updater;
+     
+   
+    double lambda = 1e-1;
+    int rank = 20;
+
+    Model* model = new Model(lambda, nRows, nCols, nExamples, rank);
+    Updater* updater = new Updater(model, trainData);
     Trainer* trainer = NULL;
     if (taskid == 0) {
-        trainer = new ServerTrainer(model, trainData, testDta);
+        trainer = new MasterTrainer(model, trainData, testData);
     }
     else {
         trainer = new WorkerTrainer(model, trainData);
     }
     TrainStatistics stats = trainer->Train(model, trainData, updater);
+   
+    // Delete new data
+    delete model;
+    delete updater;
+    delete trainer;
+    delete trainData;
+    delete testData;
     
     MPI_Finalize();
     return 0;
