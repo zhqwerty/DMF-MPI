2b64d6d0c064b720b9d126857ccac56bb9374ef1
zh <zhanghan213@163.com>
2017-11-19 15:47:02 +0800

    fix bug in gradient calculate

--- Updater/updater.h
+++ Updater/updater.h
@@ -20,6 +20,22 @@ public:
 
     Updater(){}
     virtual ~Updater(){}
+    
+    virtual void ApplyGradient(Model& model, const int row, const int col, const mat& gradXi, const mat& gradYj, const double learning_rate){
+        model.X.row(row) -= learning_rate * gradXi;
+        model.Y.col(col) -= learning_rate * gradYj;
+    }
+
+    std::pair<mat, mat> CalGradient(mat& Xi, mat& Yj){
+        std::pair<mat, mat> res;
+        mat predict = Xi * Yj;
+        double den = pow(1 + exp(predict(0, 0) * example->rating), 2);
+        mat gradXi = -exp(example->rating * predict(0, 0)) * example->rating * Yj.t() / den + model->lambda * Xi;
+        mat gradYj = -exp(example->rating * predict(0, 0)) * example->rating * Xi.t() / den + model->lambda * Yj;
+        res.first = gradXi;
+        res.second = gradYj;
+        return res;
+    }
 
     virtual void Update(mat& Xi, mat& Yj, Example* example, double learning_rate, double lambda){
         mat predict = Xi * Yj;
@@ -32,10 +48,10 @@ public:
     virtual void Update_Sig(mat& Xi, mat& Yj, Example* example, double learning_rate, double lambda){
         mat predict = Xi * Yj;
         double den = pow(1 + exp(predict(0, 0) * example->rating), 2);
-        mat gradXi = -exp(example->rating * predict(0, 0)) * Yj.t() / den + lambda * Xi;
+        mat gradXi = -exp(example->rating * predict(0, 0)) * example->rating * Yj.t() / den + lambda * Xi;
         Xi -= learning_rate * gradXi;
         predict = Xi * Yj;
-        mat gradYj = -exp(example->rating * predict(0, 0)) * Xi.t() / den + lambda * Yj;
+        mat gradYj = -exp(example->rating * predict(0, 0)) * example->rating * Xi.t() / den + lambda * Yj;
         Yj -= learning_rate * gradYj;
     }
 };
@@ -50,14 +66,15 @@ void update(Example* trainData, int nExamples, mat& X, mat& Y, double learningRa
     Y.col(trainData[randPick].col) -= learningRate * gradYj;
 }
 
-void update_sig(Example* trainData, int nExamples, mat& X, mat& Y, double learningRate, double lambda){
+void update_sig(Example* trainData, const std::vector<int>& sample, double sample_rate, int nExamples, mat& X, mat& Y, double learningRate, double lambda){
     std::random_device rd;
-    int randPick = rd() % nExamples;
-    mat predict = X.row(trainData[randPick].row) * Y.col(trainData[randPick].col);
-    double den = pow(1 + exp(predict(0, 0) * trainData[randPick].rating), 2);
-    mat gradXi = -exp(trainData[randPick].rating * predict(0, 0)) * Y.col(trainData[randPick].col).t() / den + lambda * X.row(trainData[randPick].row); 
-    X.row(trainData[randPick].row) -= learningRate * gradXi;
-    mat gradYj = -exp(trainData[randPick].rating * predict(0, 0)) * X.row(trainData[randPick].row).t() / den + lambda * Y.col(trainData[randPick].col);
-    Y.col(trainData[randPick].col) -= learningRate * gradYj;
+    int randPick = rd() % (int(nExamples * sample_rate));
+    int i = trainData[sample[randPick]].row, j = trainData[sample[randPick]].col, sign = trainData[sample[randPick]].rating;
+    mat predict = X.row(i) * Y.col(j);
+    double den = pow(1 + exp(predict(0, 0) * sign), 2);
+    mat gradXi = -exp(sign * predict(0, 0)) * sign * Y.col(j).t() / den + lambda * X.row(i); 
+    X.row(i) -= learningRate * gradXi;
+    mat gradYj = -exp(sign * predict(0, 0)) * sign * X.row(i).t() / den + lambda * Y.col(j);
+    Y.col(j) -= learningRate * gradYj;
 }
 
