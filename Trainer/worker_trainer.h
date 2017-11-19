7223722d4e519ec36dacb523d10379d62186dc39
zh <zhanghan213@163.com>
2017-11-19 15:46:32 +0800

    modify

--- Trainer/worker_trainer.h
+++ Trainer/worker_trainer.h
@@ -16,22 +16,15 @@ public:
     TrainStatistics Train(Model* model, Example* trainData, Updater* updater) override {
         TrainStatistics stats;
         //std::cout << "worker_trainer running... " <<  std::endl;
-        double flag_epoch = 0;
         double flag_break = 0;
         MPI_Status status;
-        // message: Xi, Yj, idx, learning_rate, flag_epoch, flag_break;
-        std::vector<double> message(model->rank * 2 + 4);
+        // message: Xi, Yj, idx, flag_break;
+        std::vector<double> message(model->rank * 2 + 2);
         
         while (true) { 
-            if (flag_epoch){
-                flag_epoch = 0;
-                continue;
-            }
-            if (flag_break){
-                break;
-            }
+            if (flag_break) break;
             // std::cout << "worker receive from master" << std::endl;
-            MPI_Recv(&message[0], model->rank * 2 + 4, MPI_DOUBLE, 0, 102, MPI_COMM_WORLD, &status);
+            MPI_Recv(&message[0], model->rank * 2 + 2, MPI_DOUBLE, 0, 102, MPI_COMM_WORLD, &status);
             // std::cout << "worker receive down" << std::endl;
           
             // Prase message
@@ -41,24 +34,22 @@ public:
             mat Xi = vec_2_mat(tmp_xi, 0, 1, model->rank);
             mat Yj = vec_2_mat(tmp_yj, 0, model->rank, 1);
             
-            int idx = *(message.end() - 4);
-            double learning_rate = *(message.end() - 3);
-            flag_epoch = *(message.end() - 2);
+            int idx = *(message.end() - 2);
             flag_break = *(message.end() - 1);
 
-            // Update Xi and Yj
-            updater->Update_Sig(Xi, Yj, &trainData[idx], learning_rate, model->lambda);
+            // Calculate Gradient
+            std::pair<mat, mat> gradient;
+            gradient = updater->CalGradient(Xi, Yj);
 
-            tmp_xi = mat_2_vec(Xi);
-            tmp_yj = mat_2_vec(Yj);
+            std::vector<double> gradXi = mat_2_vec(gradient.first);
+            std::vector<double> gradYj = mat_2_vec(gradient.second);
             
-            // Send (Xi, Yj, idx)
-            tmp_xi.insert(tmp_xi.end(), tmp_yj.begin(), tmp_yj.end());
-            tmp_xi.push_back(idx);
+            // Send (gradXi, gradYj, idx)
+            gradXi.insert(gradXi.end(), gradYj.begin(), gradYj.end());
+            gradXi.push_back(idx);
             // std::cout << "worker send to master" << std::endl;
-            MPI_Send(&tmp_xi[0], model->rank * 2 + 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
+            MPI_Send(&gradXi[0], model->rank * 2 + 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
             // std::cout << "worker send down" << std::endl;
-
         }
         return stats;
     } 
